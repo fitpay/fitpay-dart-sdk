70b3eae5da0cf106bf65f4d2cfce14cde7c582e3
Scott Stevelinck <scott@fit-pay.com>
2019-05-22 23:27:19 -0600

    initial template

---
+++ example/fitpay_dart_sdk_example.dart
@@ -0,0 +1,5 @@
+import 'package:fitpay_dart_sdk/fitpay.dart';
+
+main() {
+  API api = new API();
+}
---
+++ test/fitpay_dart_sdk_test.dart
@@ -0,0 +1,14 @@
+import 'package:fitpay_dart_sdk/fitpay.dart';
+import 'package:test/test.dart';
+
+void main() {
+  group('A group of tests', () {
+    API api;
+
+    setUp(() {
+      api = new API();
+    });
+
+    test('First Test', () {});
+  });
+}
